9f424627ac79c0a9e946c01eef5aa8332a09c43c
Oliver Bestwalter <oliver@bestwalter.de>
2014-08-22 16:41:16 +0200

    price_fraction as little class that generates the values from the original table in the Task description

---
+++ price_fraction.py
@@ -0,0 +1,58 @@
+"""
+A friend of mine runs a pharmacy.
+He has a specialised function in his Dispensary application which
+receives a decimal value of currency and replaces it to a standard value.
+This value is regulated by a government department.
+
+Task: Given a floating point value between 0.00 and 1.00,
+rescale according to the following table:
+"""
+
+table = """\
+>=  0.00  <  0.06  :=  0.10
+>=  0.06  <  0.11  :=  0.18
+>=  0.11  <  0.16  :=  0.26
+>=  0.16  <  0.21  :=  0.32
+>=  0.21  <  0.26  :=  0.38
+>=  0.26  <  0.31  :=  0.44
+>=  0.31  <  0.36  :=  0.50
+>=  0.36  <  0.41  :=  0.54
+>=  0.41  <  0.46  :=  0.58
+>=  0.46  <  0.51  :=  0.62
+>=  0.51  <  0.56  :=  0.66
+>=  0.56  <  0.61  :=  0.70
+>=  0.61  <  0.66  :=  0.74
+>=  0.66  <  0.71  :=  0.78
+>=  0.71  <  0.76  :=  0.82
+>=  0.76  <  0.81  :=  0.86
+>=  0.81  <  0.86  :=  0.90
+>=  0.86  <  0.91  :=  0.94
+>=  0.91  <  0.96  :=  0.98
+>=  0.96  <  1.01  :=  1.00
+"""
+
+
+class PriceRescaler(object):
+    def __init__(self, table):
+        self.upperBoundPriceTuples = self.transform_table(table)
+
+    def transform_table(self, table):
+        upperBoundPriceTuples = []
+        for line in table.splitlines():
+            interval, result = line.split(":=")
+            upperBound = interval.split('<')[1]
+            upperBoundPriceTuples.append((float(upperBound), float(result)))
+        return upperBoundPriceTuples
+
+    def rescale(self, price):
+        assert 0 <= price < 1.01, "rescales only between 0 and 1"
+        for upperBound, result in self.upperBoundPriceTuples:
+            if price < upperBound:
+                return result
+
+
+if __name__ == '__main__':
+    pt = PriceRescaler(table)
+    for dec in range(101):
+        price = float(dec) / 100
+        print("%.2f -> %.2f" % (price, pt.rescale(price)))
